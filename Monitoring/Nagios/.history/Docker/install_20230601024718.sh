7fff0b0fa5831e357893b9d36c66cdbbcdbf8c04
Oscar Macias <79873527+OMaciasd@users.noreply.github.com>
2023-06-22 04:34:30 -0500

    :fire: perf(staging) - Delete unnecesary items

--- Monitoring/Nagios/.history/Docker/install_20230601024718.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/usr env
-
-yes | apt update;
-        yes | apt upgrade;
-        yes | apt autoremove;
-        yes | apt install autoconf
--- Monitoring/Nagios/.history/Scripting/ubuntu_20230601182213.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash/usr env
-
-
-
-/usr/lib/nagios/plugins/check_tcp -H DIRECCION_IP_O_HOSTNAME -p PUERTO
